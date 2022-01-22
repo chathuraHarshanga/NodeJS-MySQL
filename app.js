899ebf1b2b6d55f3c12fc3bf57dda9c7aa805206
Chathura Harshanga <38973288+chathuraHarshanga@users.noreply.github.com>
2022-01-22 15:26:02 +0530

    select operation with sql

--- app.js
+++ app.js
@@ -9,6 +9,32 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 //MySQL
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "node_sql",
+});
+
+app.get("/", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.query("SELECT * from beers", (err, rows) => {
+      connection.release(); // return the connection to pool
+
+      if (!err) {
+        res.send(rows);
+      } else {
+        console.log(err);
+      }
+
+      // if(err) throw err
+      console.log("The data from beer table are: \n", rows);
+    });
+  });
+});
 
 //listen on port
 app.listen(PORT, () =>
